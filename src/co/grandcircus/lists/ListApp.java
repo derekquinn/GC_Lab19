ab6bf04cff18ced3cea5a349a0261d4a94445902
derekquinn <quinnderek@yahoo.com>
2019-02-06 23:19:42 -0500

    working pt 1

--- src/co/grandcircus/lists/ListApp.java
+++ src/co/grandcircus/lists/ListApp.java
@@ -1,15 +1,13 @@
 package co.grandcircus.lists;
 
-
 public class ListApp {
 // tests a link list (and array list but not today)
 	public static void main(String[] args) {
-		//testArrayList();
+		// testArrayList();
 		testLinkedList();
-	
 		
-	
-	
+		
+
 	}
 //	
 //	private static void testArrayList() {
@@ -27,21 +25,22 @@ public class ListApp {
 //		System.out.println(list.get(0));
 //		System.out.println(list.get(1));
 //	}
-	
+
 	private static void testLinkedList() {
 		MyLinkedList list = new MyLinkedList();
-		
-	//	list.addAtBeginning("B");
+
+		// list.addAtBeginning("B");
 		list.addAtBeginning("A");
-		//list.addAtEnd("C");
+		// list.addAtEnd("C");
 		list.addAtEnd("D");
-	//	list.removeFromBeginning();
-	//	list.removeFromEnd();
-		
+		// list.removeFromBeginning();
+		// list.removeFromEnd();
+		list.removeAt(1);
+
 		System.out.println(list);
-	//	System.out.println(list.size());
-	//	System.out.println(list.get(0));
-	//	System.out.println(list.get(1));
+		System.out.println(list.size());
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
 	}
 
 }
---
+++ src/co/grandcircus/lists/partTwo_A.java
@@ -0,0 +1,13 @@
+package co.grandcircus.lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class partTwo_A {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10));
+
+	}
+
+}
