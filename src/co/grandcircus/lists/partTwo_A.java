daa376c78536819704a51ad749eb0bdd1b0dbc32
derekquinn <quinnderek@yahoo.com>
2019-02-07 01:50:12 -0500

    almost done

--- src/co/grandcircus/lists/partTwo_A.java
+++ src/co/grandcircus/lists/partTwo_A.java
@@ -15,12 +15,12 @@ public class partTwo_A {
 // temporarily holds the frequency values before written to array
 		int t = 0;
 
-// lucky numbers will be stored and searched
+// lucky numbers will be stored and searchedddd
 		Scanner scnr = new Scanner(System.in);
 // this app is being written for Tupperware to help their party hosts make bank.
 // Hypothetically.
 		System.out.println("WELCOME TO THE TUPPERWARE PARTY GAME CENTER (BETA RELEASE 0.0.2) \n");
-		System.out.println("Please take turns entering your favorite number between 1-10");
+		System.out.println("Please take turns entering your favorite number (between 1-10)");
 // collect the lucky numbers
 		for (int i = 0; i < luckyNumbers.length; i++) {
 
@@ -33,16 +33,16 @@ public class partTwo_A {
 			t = luckyNumbers[i];
 			numberFreq[t]++;
 		}
-// print out the number frequencies , profit
+
 		
 		System.out.println("==== LUCKY NUMBERS BY TUPPERWARE ====");
 		System.out.println("============= RESULTS ===============");
 		for (int i = 0; i < numberFreq.length; i++) {
-
+// print out the number frequencies , profit
 			if (numberFreq[i] > 0 && numberFreq[i] == 1) {
-				System.out.printf("%d occurs %d time\n", i, numberFreq[i]);
+				System.out.printf("%d OCCURS %d TIME\n", i, numberFreq[i]);
 			} else if (numberFreq[i] >= 2) {
-				System.out.printf("%d occurs %d times\n", i, numberFreq[i]);
+				System.out.printf("%d OCCURS %d TIMES\n", i, numberFreq[i]);
 			}
 
 		}
@@ -51,7 +51,7 @@ public class partTwo_A {
 		
 		System.out.println("Despite their kitschy reputation, Tupperware parties take place in more than 100 countries, \nwith 520,000 happening every year in France alone.");
 
-	} // END MAIN METHOD MAN
+	} // END MAIN METHOD
 	
 
 
